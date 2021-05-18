12e6601af653ee282290a7226bdaeb0c9bd970cf
Kasiliana Oliveira <kasilianaoliveira@gmail.com>
2021-05-18 11:00:55 -0300

    pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import Logon from './pages/Logon';
+import './global.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Logon />
+    </>
   );
 }
 
